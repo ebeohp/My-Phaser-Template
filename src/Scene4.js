2bc0f665801db6e28fb3edbdc5de881ef5123137
Phoebe Lin <phoebe1318@yahoo.com>
2020-06-24 10:51:07 -0500

    with npm

---
+++ src/Scene4.js
@@ -0,0 +1,22 @@
+class Scene4 extends Phaser.Scene{
+    constructor(){
+        super("winGame");
+    }
+    init(){
+        
+    }
+    preload(){
+
+    }
+    
+    create(){
+        
+    }
+    update()
+    {
+         
+    }
+    restartGame(){
+        this.scene.start("bootGame");
+    }
+}
